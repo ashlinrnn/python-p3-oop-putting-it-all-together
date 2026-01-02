0c0fb7f2e8ccb3a47b54fa78f66529f3670cd7ac
Ashlin Rofina <ashlinrofina@gmail.com>
2026-01-02 13:17:53 +0300

    adds codes to the fikes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
+class Shoe: 
+    def __init__(self, brand, size): 
+        self.brand = brand 
+        self.condition = "New" # Set this before size to ensure it exists
+        self.size = size  
 
-class Shoe:
-    pass
+    @property 
+    def size(self): 
+        return self._size  
+
+    @size.setter 
+    def size(self, value):  
+        if not isinstance(value, int):
+            print("size must be an integer")
+            
+            
+        self._size = value
+
+    def cobble(self): 
+        self.condition = "New"  
+        print("Your shoe is as good as new!")
